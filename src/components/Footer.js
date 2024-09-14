2307781984bb88537079aed720e4d133f611ce47
sportyisus <diazmartinmail@gmail.com>
2024-09-14 12:15:55 +0200

    Agregando políticas de privacidad

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,15 +3,16 @@ import React from 'react';
 // Importamos los estilos específicos del footer
 import './Footer.css';
 // Importamos el logo nuevo
-import logo from '../assets/LOGO2.png'; // Asegúrate de que el logo esté en esta ruta
+import logo from '../assets/logodeveloper.png'; // Asegúrate de que el logo esté en esta ruta
 
 function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
         <img src={logo} alt="Logo Jesús" className="footer-logo" />
-        <p>© {new Date().getFullYear()} Jesús Díaz - Todos los derechos reservados</p>
+        <p>© - {new Date().getFullYear()} JDM - All Rights Reserved</p>
       </div>
+      <a href="/privacy-policy.html" className="privacy-link">Políticas de Privacidad</a>
     </footer>
   );
 }
